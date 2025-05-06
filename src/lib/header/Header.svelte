4d37adb44e4c0604d1e545a494ff1cd82769e652
cypress <2271478667@qq.com>
2025-05-07 00:58:45 +0800

    automatically commit.

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -105,18 +105,6 @@
 		height: 100%;
 	}
 
-	li.active::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--accent-color);
-	}
-
 	nav a {
 		display: flex;
 		height: 100%;
